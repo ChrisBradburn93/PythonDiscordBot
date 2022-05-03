b0a6f10fef5f2c82e5a2ba88748b075025922cf3
Chris Bradburn <chribradburn93@gmail.com>
2022-05-03 16:08:52 +0100

    .

--- QuestHook.py
+++ QuestHook.py
@@ -1,12 +1,16 @@
 import json
 import random
 
-with open('places.json') as d:
-        data = json.load(d)
-P = data["Place"]
-AT = data["AdjectiveTense"]
-AO = data["AdjectiveOwner"]
-EP = data["EnemyPrefix"]
-ET = data["EnemyType"]
 
-print ("You are tasked with seeking out the disturbance at the " + (random.choice(AO)+" "+(random.choice(AT)+" "+(random.choice(P)+". There have been rumours that a "+(random.choice(EP)+" "+(random.choice(ET))+ " has been spotted in the area.")))))
+
+def AdventureCall():
+
+    with open('places.json') as d:
+        data = json.load(d)
+    P = data["Place"]
+    AT = data["AdjectiveTense"]
+    AO = data["AdjectiveOwner"]
+    EP = data["EnemyPrefix"]
+    ET = data["EnemyType"]
+
+    return "You are tasked with seeking out the disturbance at the " + (random.choice(AO)+" "+(random.choice(AT)+" "+(random.choice(P)+". There have been rumours that a "+(random.choice(EP)+" "+(random.choice(ET))+ " has been spotted in the area."))))
--- discordconnection.py
+++ discordconnection.py
@@ -1,15 +1,52 @@
 # bot.py
 import os
+from urllib import response
 import discord
+import random
+import json
 from dotenv import load_dotenv
+from QuestHook import AdventureCall
+
+intents = discord.Intents.default()
+intents.members = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+    f'Hi {member.name}, welcome to the server. Play nice or you''ll get got!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'I want to go on an adventure!':
+        response = AdventureCall()
+        await message.channel.send(response)
+
 
 client.run(TOKEN)
