b12ffdc790c1ae67a2539ba0a726ed65595e7dd3
Chris Bradburn <chribradburn93@gmail.com>
2022-05-08 19:05:40 +0100

    .

--- NPCFunction.py
+++ NPCFunction.py
@@ -1,4 +1,5 @@
 import json
+from msilib.schema import Class
 import random
 
 
@@ -7,7 +8,10 @@ def NPCCall():
 
     with open('NPC.json') as n:
         data = json.load(n)
-    FN = data["FName"]
-    LN = data["LName"]
+    Male_FN = data["Dragonborn_Male_Fname"]
+    Female_FN = data["Dragonborn_Female_Fname"]
+    LN = data["Dragonborn_Lname"]
+    CL = data["Class"]
 
-    return (random.choice(FN)+" "+ (random.choice(LN)+" is a NPC "))
+    return (random.choice(Male_FN)+" "+ (random.choice(LN)+" is a "+(random.choice(CL))))
+ 
